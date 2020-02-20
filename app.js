2620c32357209ef6d8ab2f7cb31fc5f19ca836ba
Aiden Berzins <aiden.berzins@hotmail.com>
2020-02-20 07:37:26 -0700

    functioning basic calculator

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const calculator = {
 const inputDigit = digit => {
   const { displayValue, waitingForSecondOperand } = calculator;
 
-  if (waitingForSecondOperand) {
+  if (waitingForSecondOperand === true) {
     calculator.displayValue = digit;
     calculator.waitingForSecondOperand = false;
   } else {
@@ -25,16 +25,37 @@ const inputDecimal = dot => {
   }
 };
 
-const handleOperator = nextOperator => {
+function handleOperator(nextOperator) {
   const { firstOperand, displayValue, operator } = calculator;
-  // const inputValue = parseFloat(displayValue);
+  const inputValue = parseFloat(displayValue);
+
+  if (operator && calculator.waitingForSecondOperand) {
+    calculator.operator = nextOperator;
+    console.table(calculator);
+    return;
+  }
 
   if (firstOperand == null) {
-    calculator.firstOperand = parseFloat(displayValue);
+    calculator.firstOperand = inputValue;
+  } else if (operator) {
+    const currentValue = firstOperand || 0;
+    const result = performCalculation[operator](currentValue, inputValue);
+
+    calculator.displayValue = String(result);
+    calculator.firstOperand = result;
   }
+
   calculator.waitingForSecondOperand = true;
   calculator.operator = nextOperator;
-  console.table(calculator);
+  console.log(calculator);
+}
+
+const performCalculation = {
+  "/": (firstOperand, secondOperand) => firstOperand / secondOperand,
+  "*": (firstOperand, secondOperand) => firstOperand * secondOperand,
+  "+": (firstOperand, secondOperand) => firstOperand + secondOperand,
+  "-": (firstOperand, secondOperand) => firstOperand - secondOperand,
+  "=": (firstOperand, secondOperand) => secondOperand
 };
 
 const resetCalculator = () => {
