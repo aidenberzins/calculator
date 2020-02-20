16195ff98bab18daf989be86e49dd3951118769e
Aiden Berzins <aiden.berzins@hotmail.com>
2020-02-19 20:47:01 -0700

    restarted with new calculator

--- app.js
+++ app.js
@@ -1,69 +1,13 @@
-// Buttons
-const num1 = document.querySelector("#num1");
-const num2 = document.querySelector("#num2");
-const num3 = document.querySelector("#num3");
-const num4 = document.querySelector("#num4");
-const num5 = document.querySelector("#num5");
-const num6 = document.querySelector("#num6");
-const num7 = document.querySelector("#num7");
-const num8 = document.querySelector("#num8");
-const num9 = document.querySelector("#num9");
-const num0 = document.querySelector("#num0");
+const calculator = {
+  displayValue: "0",
+  firstOperand: null,
+  waitingForSecondOperand: false,
+  operator: null
+};
 
-const decimal = document.querySelector("#decimal");
-const multiply = document.querySelector("#multiply");
-const divide = document.querySelector("#divide");
-const subtract = document.querySelector("#subtract");
-const add = document.querySelector("#add");
+function updateDisplay() {
+  const display = document.querySelector("#screen");
+  display.value = calculator.displayValue;
+}
 
-const equate = document.querySelector("#equate");
-
-num1.addEventListener("click", function() {
-  console.log("num1");
-});
-num2.addEventListener("click", function() {
-  console.log("num2");
-});
-num3.addEventListener("click", function() {
-  console.log("num3");
-});
-num4.addEventListener("click", function() {
-  console.log("num4");
-});
-num5.addEventListener("click", function() {
-  console.log("num5");
-});
-num6.addEventListener("click", function() {
-  console.log("num6");
-});
-num7.addEventListener("click", function() {
-  console.log("num7");
-});
-num8.addEventListener("click", function() {
-  console.log("num8");
-});
-num9.addEventListener("click", function() {
-  console.log("num9");
-});
-num0.addEventListener("click", function() {
-  console.log("num0");
-});
-
-decimal.addEventListener("click", function() {
-  console.log("decimal");
-});
-multiply.addEventListener("click", function() {
-  console.log("multiply");
-});
-divide.addEventListener("click", function() {
-  console.log("divide");
-});
-subtract.addEventListener("click", function() {
-  console.log("subtract");
-});
-add.addEventListener("click", function() {
-  console.log("add");
-});
-equate.addEventListener("click", function() {
-  console.log("equate");
-});
+updateDisplay();
