9c2818fbbe93eb98720c749fc943f12e462bb35f
Aiden Berzins <aiden.berzins@hotmail.com>
2020-02-19 21:05:52 -0700

    added event listener to check which key is being pressed

--- app.js
+++ app.js
@@ -11,3 +11,37 @@ function updateDisplay() {
 }
 
 updateDisplay();
+
+const keys = document.querySelector(".calculator-keys");
+keys.addEventListener("click", event => {
+  const { target } = event;
+  if (!target.matches("button")) {
+    return;
+  }
+
+  if (target.classList.contains("operator")) {
+    console.log(
+      `%c operator ${target.value}`,
+      `background: #353535; color: #bada55; padding: 10px;`
+    );
+    return;
+  }
+
+  if (target.classList.contains("decimal")) {
+    console.log(`%c decimal${target.value}`, `color: #bada55; padding: 10px;`);
+    return;
+  }
+
+  if (target.classList.contains("all-clear")) {
+    console.log(
+      `%c clear, ${target.value}`,
+      `background: #222; color: yellow; padding: 10px;`
+    );
+    return;
+  }
+
+  console.log(
+    `%c digit, ${target.value}`,
+    `background: #222; color: #bada55; padding: 10px;`
+  );
+});
